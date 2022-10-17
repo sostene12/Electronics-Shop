e594122ab62ab0953acc9375a3d63baf75133850
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2022-10-17 19:15:40 +0200

    --ammend

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,6 @@
 import Product from "../model/Product";
 import cloudinary from "../helpers/cloudinary";
+import path from "path";
 
 class ProductController{
     static async createProduct (req,res){
@@ -29,12 +30,30 @@ class ProductController{
             res.status(404).json({error:error.message})
         }
     }
+    static async uploadFile (req,res){
+        try {
+            const file = req.files.file[0].path;
+            const link = cloudinary.uploader.upload(file);
+            res.status(200).json({message:"file uploaded",link:link.secure_url});
+        } catch (error) {
+            res.json({error:error.message});
+        }
+    }
     static async deleteProduct(req,res){
         try {
             await Product.findByIdAndDelete({_id:req.params.id});
             res.status(200).json("Product deleted");
         } catch (error) {
             res.status(401).json({error:error.message});
+        }
+    }
+
+    static async updateProduct(req,res){
+        try {
+            const updatedProduct = await Product.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true});
+            res.status(200).json(updatedProduct);
+        } catch (error) {
+            res.status(404).json({error:error.message});
         }
     }
 }
